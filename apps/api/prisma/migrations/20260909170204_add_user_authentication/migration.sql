577254d59cf956666a4b853478c01d2fb9914f31
Tomás Martín <tmartindsgn@gmail.com>
2026-09-09 19:16:41 +0200

    feat: add user authentication foundation

---
+++ apps/api/prisma/migrations/20260909170204_add_user_authentication/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Project` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
